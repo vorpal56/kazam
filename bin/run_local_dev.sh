eac859e563ee855e43ebfc064f5db025d88c88c2
niknah <github@mail.niknah.com>
2021-03-19 20:36:17 +1100

    v1.5.  Broadcast + webcam buttons.

--- bin/run_local_dev.sh
+++ bin/run_local_dev.sh
@@ -2,6 +2,7 @@
 cd data; 
 cp -ua icons hicolor
 cd hicolor
+ln -s . scalable/apps >/dev/null 2>&1
 find -iname '*.png' -type f -printf "ln -s '%f' '%P-symbolic'\n" | sed -e 's#.png-symbolic#-symbolic.png#' | sh >/dev/null 2>&1
 
 cd ../../bin
