b10e8041c4dd1412e3dd53f92d1690373b4811db
niknah <github@mail.niknah.com>
2021-03-19 11:41:38 +1100

    bin/run_local_dev.sh, symlink .svg files too.

--- bin/run_local_dev.sh
+++ bin/run_local_dev.sh
@@ -2,7 +2,7 @@
 cd data; 
 cp -ua icons hicolor
 cd hicolor
-find -iname '*.png' -type f -printf "ln -s '%f' '%P-symbolic'\n" | sed -e 's#.png-symbolic#-symbolic.png#' | sh >/dev/null 2>&1
+find -type f -printf "%f,,,%P-sym\n" | perl -ne 's#\s*##; @x=split(/,,,/);if($x[0]!~m#-symbolic# && $x[1]=~s#\.(svg|png)-sym$#-symbolic\.\1#) { symlink($x[0],$x[1]); }' 
 
 cd ../../bin
 export PYTHONPATH=..
